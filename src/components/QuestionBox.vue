46c4e438702a07bef9196d165d72f7c3c3eca605
tungnm97 <tungnguyen@x3english.com>
2020-06-22 17:25:41 +0700

    first commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron lead="Bootstrap v4 Components for Vue.js 2">
+            
+            <template v-slot:lead>
+                Questions
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            Answers
+            </p>
+
+            <b-button variant="primary" href="#">Subbmit</b-button>
+            <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
