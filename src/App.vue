90fb56b9e045dacd68398f966bda7fff2faae44c
tungnm97 <tungnguyen@x3english.com>
2020-06-23 10:17:24 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <Header />
+    <Header 
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
-            <QuestionBox />
+            <QuestionBox 
+              v-if="questions.length"
+              :currentQuestion="questions[index]"
+              :next="next"
+              :increment="increment"
+            />
         </b-col>
       </b-row>
     </b-container>
@@ -20,6 +28,37 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
+    }
+  },
+  mounted: function () {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results
+      console.log(this.questions)
+    })
   }
 }
 </script>
